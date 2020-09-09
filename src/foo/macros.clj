a3d3a83a61ad5111f564b0a51a274d6a733446d1
Juan Monetta <jpmonettas@gmail.com>
2020-09-09 18:01:09 -0300

    A possible solution

--- src/foo/macros.clj
+++ src/foo/macros.clj
@@ -1,4 +1,25 @@
-(ns foo.macros)
+(ns foo.macros
+  (:require [cljs.analyzer :as ana]))
+
+(def ^:dynamic *environment*)
+
+
+(defn target-from-env [env]
+  (if (contains? env :js-globals)
+    :cljs
+    :clj))
+
+(defn hacked-macroexpand-1 [form]
+  ((case (target-from-env *environment*)
+     :cljs (partial ana/macroexpand-1 *environment*)
+     :clj  macroexpand) form))
+
+(defn hacked-macroexpand [form]
+  (let [ex (hacked-macroexpand-1 form)]
+      (if (identical? ex form)
+        form
+        (hacked-macroexpand ex))))
 
 (defmacro m [form]
-  `(quote ~(macroexpand form)))
+  (binding [*environment* &env]
+    `(quote ~(hacked-macroexpand form))))
